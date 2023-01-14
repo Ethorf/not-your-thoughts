444e713e3b2dc4deb30e0cba8a73df51850503ef
Eric Thorfinnson <ethorf@gmail.com>
2023-01-13 19:20:53 -0600

    add a build folder

---
+++ client/build/precache-manifest.9652e5483d2415c36039369691fe6f6c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce742d960b5820b7590f5bd2fa6fea79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc17a8617143b13cc55",
+    "url": "/static/css/main.fb33e1e8.chunk.css"
+  },
+  {
+    "revision": "8905c4b4ae685a67ac7c",
+    "url": "/static/js/2.7df16af3.chunk.js"
+  },
+  {
+    "revision": "9cc17a8617143b13cc55",
+    "url": "/static/js/main.39305d38.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "2245a95ad122be3d683b75f1150e7822",
+    "url": "/static/media/NewCrawBox-Inverted-Bottom.2245a95a.gif"
+  },
+  {
+    "revision": "0ee6bce3b7258c682b8c5099d397b0af",
+    "url": "/static/media/NewCrawBox-Inverted-Left.0ee6bce3.gif"
+  },
+  {
+    "revision": "4cfc88619062470b183c23ba33fc76b5",
+    "url": "/static/media/NewCrawBox-Inverted-Right.4cfc8861.gif"
+  },
+  {
+    "revision": "434be8554d07ba0cd2400f4bbc3ef278",
+    "url": "/static/media/NewCrawBox-Inverted-Top.434be855.gif"
+  },
+  {
+    "revision": "b0bbe1007146174deedceb89e50c8e96",
+    "url": "/static/media/NewPillarLeft-2-inverted.b0bbe100.png"
+  },
+  {
+    "revision": "fb745d8e053953aee93eeb8c847a37bd",
+    "url": "/static/media/NewPillarLeft-2.fb745d8e.png"
+  },
+  {
+    "revision": "a10474a9c74e9cbfc9f7cb1772c817d9",
+    "url": "/static/media/NewPillarTop-4-inverted.a10474a9.png"
+  },
+  {
+    "revision": "f442f83d64d3bb6f1d11658964e38f5b",
+    "url": "/static/media/NewPillarTop-Round-4.f442f83d.png"
+  },
+  {
+    "revision": "e566a6e976191447188ab9de1219dcf2",
+    "url": "/static/media/Not-Your-Thoughts-100-progressSound.e566a6e9.mp3"
+  },
+  {
+    "revision": "0d214aa3b38ff2a54fe71683181cd3ae",
+    "url": "/static/media/Not-Your-Thoughts-25-progressSound.0d214aa3.mp3"
+  },
+  {
+    "revision": "b2c8a6ab9a6d9fbd83a175130b09e4f0",
+    "url": "/static/media/Not-Your-Thoughts-50-progressSound.b2c8a6ab.mp3"
+  },
+  {
+    "revision": "a9b12d8925e7179be3747c603f7bf291",
+    "url": "/static/media/Not-Your-Thoughts-75-progressSound.a9b12d89.mp3"
+  },
+  {
+    "revision": "b9a7cef0b25bde79cb7f74a1e39b9f08",
+    "url": "/static/media/Not-Your-Thoughts-Ambient-Track-1.b9a7cef0.mp3"
+  },
+  {
+    "revision": "85145a599934639444760d4e4673c297",
+    "url": "/static/media/SpikyCrawBox-Bottom-1.85145a59.gif"
+  },
+  {
+    "revision": "0848f74178cfec2d30ba4ac1dd907eb4",
+    "url": "/static/media/SpikyCrawBox-Left-1.0848f741.gif"
+  },
+  {
+    "revision": "fa702e8b394e946505faabf4f5ff1647",
+    "url": "/static/media/SpikyCrawBox-Right-1.fa702e8b.gif"
+  },
+  {
+    "revision": "7fbd4b74c03c416df131f7d8268e612d",
+    "url": "/static/media/SpikyCrawBox-Top-1.7fbd4b74.gif"
+  },
+  {
+    "revision": "67375bd888fdf5a615cf8eda48d2d6e2",
+    "url": "/static/media/background-texture-bigPan-white-blur.67375bd8.png"
+  },
+  {
+    "revision": "1b56423c7565048541d01cdbdcd035bb",
+    "url": "/static/media/bgImg-donut1-inverted.1b56423c.png"
+  },
+  {
+    "revision": "111815b6daf4ab6b1b595e716fdbab25",
+    "url": "/static/media/bgImg-donut1.111815b6.png"
+  },
+  {
+    "revision": "bd69c16a4da953343dba68318bcfb9ae",
+    "url": "/static/media/loading-spinner-black.bd69c16a.gif"
+  },
+  {
+    "revision": "7581a0f0cf534a78c570e9253c4abb76",
+    "url": "/static/media/pause-icon-black.7581a0f0.png"
+  },
+  {
+    "revision": "7e414957bf1999ac9ab95b74c34c8ca4",
+    "url": "/static/media/play-icon-black.7e414957.png"
+  },
+  {
+    "revision": "851d94ac5e31253a8a5ef372c171b19d",
+    "url": "/static/media/speaker.851d94ac.png"
+  }
+]);
