340de3b809245f6056acb3f46c572974a062d16e
William Purcell <wedozeez@gmail.com>
2024-04-02 13:01:43 -0400

    slight update to authActions to reflect a suggestion in the PR

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -66,13 +66,6 @@ export const login = (email, password) => async (dispatch) => {
     })
     return res.data
   } catch (err) {
-    // I wonder if this error handling is an artifact of our pre-postgres DB, as `errors` is null even if we have errors
-
-    // const errors = err.response.data.errors
-
-    // if (errors) {
-    //   errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
-    // }
     dispatch({
       type: LOGIN_FAIL,
     })
