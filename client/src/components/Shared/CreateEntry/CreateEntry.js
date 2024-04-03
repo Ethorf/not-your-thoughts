742e44919e2bbb2b2c4296e56e832d0b7908d9e1
Eric Thorfinnson <ethorf@gmail.com>
2024-04-03 08:08:31 -0500

    basic setup for richtext with quill

--- client/src/components/Shared/CreateEntry/CreateEntry.js
+++ client/src/components/Shared/CreateEntry/CreateEntry.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
 import { setContent, setWordCount, setCharCount } from '../../../redux/reducers/currentEntryReducer' // Replace with the correct path
 import { ENTRY_TYPES } from '../../../constants/entryTypes'
 
 import styles from './CreateEntry.module.scss'
+import './CustomQuillStyles.scss'
 
 const CreateEntry = ({ type }) => {
   const dispatch = useDispatch()
   const { content } = useSelector((state) => state.currentEntry)
+  const [toolbarVisible, setToolbarVisible] = useState(false)
 
   const handleContentChange = (e) => {
-    dispatch(setContent(e.target.value))
+    dispatch(setContent(e))
 
     // Calculate word count
     const words = content.split(/\s+/).filter((word) => word.length > 0)
@@ -23,13 +29,30 @@ const CreateEntry = ({ type }) => {
     [ENTRY_TYPES.NODE]: 'Start node here...',
     [ENTRY_TYPES.JOURNAL]: 'Note those thoughts here...',
   }
+
+  const toolBarModules = {
+    toolbar: [
+      [],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link'],
+      [],
+    ],
+  }
+
   return (
-    <textarea
-      placeholder={PLACEHOLDER_COPY[type]}
-      className={styles.textArea}
-      value={content}
-      onChange={handleContentChange}
-    />
+    <>
+      <button onClick={() => setToolbarVisible(!toolbarVisible)}>Toggle Toolbar</button>
+      <div className={styles.wrapper}>
+        <ReactQuill
+          className={`textArea ${toolbarVisible ? 'toolbar-visible' : 'toolbar-hidden'}`}
+          modules={toolBarModules}
+          placeholder={PLACEHOLDER_COPY[type]}
+          value={content}
+          onChange={(e) => handleContentChange(e)}
+        />
+      </div>
+    </>
   )
 }
 
