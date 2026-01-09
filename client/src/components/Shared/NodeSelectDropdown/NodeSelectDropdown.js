92bb18ffc83e5ec3ad1a5650aac6df73a736b557
Eric Thorfinnson <ethorf@gmail.com>
2026-01-09 09:35:46 -0500

    bunch of updates

--- client/src/components/Shared/NodeSelectDropdown/NodeSelectDropdown.js
+++ client/src/components/Shared/NodeSelectDropdown/NodeSelectDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import classNames from 'classnames'
 import { useSelector } from 'react-redux'
 
@@ -7,39 +7,46 @@ import useNodeEntriesInfo from '@hooks/useNodeEntriesInfo'
 
 import styles from './NodeSelectDropdown.module.scss'
 
-function NodeSelectDropdown({ inputValue, setInputValue, className, onSelect, onChange }) {
+function NodeSelectDropdown({ className, onSelect, onChange }) {
   const nodeEntriesInfo = useNodeEntriesInfo()
   const { entryId } = useSelector((state) => state.currentEntry)
   const { connections } = useSelector((state) => state.connections)
 
-  const handleOnChange = (value) => {
-    onChange(value)
-
-    const foundEntry = nodeEntriesInfo?.find((x) => x.title.toLowerCase() === value.toLowerCase())
-    if (foundEntry) {
-      onSelect(foundEntry.id)
-    }
-  }
+  const [inputValue, setInputValue] = useState('')
 
   // Filter out the current entry and already-connected nodes
-  const filterOptions = (options, connections) => {
+  const filterOptions = useCallback((options, conns) => {
     if (!options) return []
-    const connectionIds = connections?.flatMap((conn) => [conn.foreign_entry_id, conn.primary_entry_id]) || []
+    const connectionIds = conns?.flatMap((conn) => [conn.foreign_entry_id, conn.primary_entry_id]) || []
 
     return options.filter((option) => {
       return !connectionIds.includes(option.id)
     })
-  }
+  }, [])
 
   const filteredNodeEntriesInfo = filterOptions(nodeEntriesInfo?.filter((x) => x?.id !== entryId) || [], connections)
-  console.log('<<<<<< inputValue >>>>>>>>> is: <<<<<<<<<<<<')
-  console.log(inputValue)
-  console.log('<<<<<< inputValue >>>>>>>>> is: <<<<<<<<<<<<')
-  console.log(inputValue)
+
+  const handleOnChange = (value) => {
+    onChange?.(value)
+    // Don't call onSelect here - let handleOnSubmit handle it to avoid double-calls
+  }
+
+  const handleOnSubmit = (selectedTitle) => {
+    if (!selectedTitle) return
+
+    // Search in all nodes (case-insensitive) to find the entry
+    const foundEntry = nodeEntriesInfo?.find((x) => x.title?.toLowerCase() === selectedTitle?.toLowerCase())
+
+    if (foundEntry) {
+      onSelect?.(foundEntry.id)
+    }
+  }
+
   return (
     <div className={classNames(styles.wrapper, className)}>
       <DefaultAutoCompleteDropdown
         onChange={handleOnChange}
+        onSubmit={handleOnSubmit}
         placeholder={'Select or create node to connect...'}
         inputValue={inputValue}
         setInputValue={setInputValue}
